bfc7241ade76b525c023e6b4fc1b356a42107cac
Roberto Rocha <robrs.web@gmail.com>
2022-04-26 14:37:21 -0300

    Adicionando instruçoes de uso

--- index.php
+++ index.php
@@ -3,8 +3,32 @@
 <head>
     <meta charset="utf-8"/>
     <title>Integração API Clicksign</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
+<div class="container">
+    <h2>Instruções de uso</h2>
+
+    <ul>
+        <li>
+            Importar a classe AppClient para dentro do projeto.
+        </li>
+        <li>
+            Criar uma instãncia da classe AppClient passando no construtor duas informações parâmetro 1 Token de acesso, parâmetro 2 âmbiente de desenvolvimento que pode ser Test ou production.
+        </li>
+        <li>
+            ex: $clicksign = new AppCliente('01464e1616e3fdd5c60c0cc5516c1d1454cc4185','Test')
+        </li>
+    </ul>
+
+    <div class="row">
+
+    </div>
+
+</div>
+
 </body>
 </html>
 
